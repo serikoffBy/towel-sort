6aaf054596bb21966daa8c27c5518c9db12f8a51
serikoffby <kornaush@gmail.com>
2020-10-06 13:44:52 +0300

    fix: update function

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 
 module.exports = function towelSort (matrix) {
   array = []
-  if(matrix.length > 0) {
+  if(matrix.length > 0 && matrix) {
     matrix.forEach((element, index) => {
       if(index % 2 === 0) {
         array = [...matrix];
