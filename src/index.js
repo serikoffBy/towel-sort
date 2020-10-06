97500ae687f1da4a2b56094e416937e669759f4c
serikoffby <kornaush@gmail.com>
2020-10-06 13:39:33 +0300

    init: start task git status
    sad
    
    asd
    as

--- src/index.js
+++ src/index.js
@@ -2,5 +2,17 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  array = []
+  if(matrix.length > 0) {
+    matrix.forEach((element, index) => {
+      if(index % 2 === 0) {
+        array = [...matrix];
+      }
+      else {
+        array = [...matrix.reverse()];
+      }
+    });
+  }
+  else return [];
+  return array;
 }
